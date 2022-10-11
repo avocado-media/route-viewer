a206a304e7eb6d6b98264a1d2f57f7326c460fe6
Sjors Eveleens <sjorspieter@live.nl>
2022-10-11 14:38:39 +0200

    fix: esc to blur input

--- resources/views/routeViewer.blade.php
+++ resources/views/routeViewer.blade.php
@@ -52,85 +52,79 @@
 </head>
 
 <body class="bg-[#171923] p-10 text-gray-200 antialiased">
-<div class="mx-auto container">
-    <section x-data="loadRoutes">
-        <div class="flex justify-between mb-4">
-            <h1 class="font-display text-3xl font-bold">Routes</h1>
+    <div class="mx-auto container">
+        <section x-data="loadRoutes">
+            <div class="flex justify-between mb-4">
+                <h1 class="font-display text-3xl font-bold">Routes</h1>
 
-            <div
-                class="relative max-w-xs w-full flex items-center ring-1 rounded-lg ring-slate-700 focus-within:ring-slate-500 transition-colors">
-                <input type="text" name="search" placeholder="Search..."
-                       class="block w-full rounded-md bg-transparent border-0 pr-12 shadow-sm focus:ring-0 sm:text-sm peer"
-                       x-ref="inputSearch" x-model="search" @keydown.window.prevent.cmd.k="$refs.inputSearch.focus()">
-                <kbd
-                    class="h-6 top-1/2 mr-1.5 -translate-y-1/2 absolute inset-y-0 right-0 flex py-1.5 items-center rounded border border-slate-700 px-2 font-sans text-sm font-medium text-slate-700 peer-focus:text-slate-500 peer-focus:border-slate-500 transition-colors">⌘K</kbd>
+                <div class="relative max-w-xs w-full flex items-center ring-1 rounded-lg ring-slate-700 focus-within:ring-slate-500 transition-colors">
+                    <input type="text" name="search" placeholder="Search..." class="block w-full rounded-md bg-transparent border-0 pr-12 shadow-sm focus:ring-0 sm:text-sm peer" x-ref="inputSearch" x-model="search" @keydown.window.prevent.cmd.k="$refs.inputSearch.focus()" @keydown.window.prevent.esc="$refs.inputSearch.blur()">
+                    <kbd class="h-6 top-1/2 mr-1.5 -translate-y-1/2 absolute inset-y-0 right-0 flex py-1.5 items-center rounded border border-slate-700 px-2 font-sans text-sm font-medium text-slate-700 peer-focus:text-slate-500 peer-focus:border-slate-500 transition-colors">⌘K</kbd>
+                </div>
             </div>
-        </div>
 
-        <div class="overflow-hidden rounded-lg shadow ring-1 ring-slate-700">
-            <div class="overflow-x-auto">
-                <table class="bg-dark-gray min-w-full divide-y divide-slate-700">
-                    <thead class="text-left text-sm font-medium">
-                    <tr>
-                        <th scope="col" class="py-3.5 pl-4 pr-3 sm:pl-6">Methods</th>
-                        <th scope="col" class="px-3 py-3.5">Path</th>
-                        <th scope="col" class="px-3 py-3.5">Name</th>
-                        <th scope="col" class="px-3 py-3.5">Action</th>
-                        <th scope="col" class="py-3.5 pl-3 pr-4">Middleware</th>
-                    </tr>
-                    </thead>
+            <div class="overflow-hidden rounded-lg shadow ring-1 ring-slate-700">
+                <div class="overflow-x-auto">
+                    <table class="bg-dark-gray min-w-full divide-y divide-slate-700">
+                        <thead class="text-left text-sm font-medium">
+                            <tr>
+                                <th scope="col" class="py-3.5 pl-4 pr-3 sm:pl-6">Methods</th>
+                                <th scope="col" class="px-3 py-3.5">Path</th>
+                                <th scope="col" class="px-3 py-3.5">Name</th>
+                                <th scope="col" class="px-3 py-3.5">Action</th>
+                                <th scope="col" class="py-3.5 pl-3 pr-4">Middleware</th>
+                            </tr>
+                        </thead>
 
-                    <tbody class="font-gray-600 divide-y divide-slate-700 text-sm">
-                    <template x-for="route in filteredRoutes">
-                        <tr>
-                            <td class="whitespace-nowrap py-4 pl-4 pr-3 sm:pl-6">
-                                <ul class="flex">
-                                    <template x-for="method in route.methods">
-                                        <li x-text="method" class="badge mr-2 last:mr-0"
-                                            :class="'badge--' + method.toLowerCase()"></li>
-                                    </template>
-                                </ul>
-                            </td>
-                            <td class="whitespace-nowrap px-3 py-4" x-text="route.uri"/>
-                            <td class="whitespace-nowrap px-3 py-4" x-text="route.name"/>
-                            <td class="whitespace-nowrap px-3 py-4 font-mono" x-text="route.action"/>
-                            <td class="whitespace-nowrap py-4 pl-3 pr-4">
-                                <ul class="flex">
-                                    <template x-for="method in route.middleware">
-                                        <li x-text="method"
-                                            class="mr-1 last:mr-0 after:content-[','] last:after:content-['']"></li>
-                                    </template>
-                                </ul>
-                            </td>
-                        </tr>
-                    </template>
-                    </tbody>
-                </table>
+                        <tbody class="font-gray-600 divide-y divide-slate-700 text-sm">
+                            <template x-for="route in filteredRoutes">
+                                <tr class="font-mono">
+                                    <td class="whitespace-nowrap py-4 pl-4 pr-3 sm:pl-6">
+                                        <ul class="flex">
+                                            <template x-for="method in route.methods">
+                                                <li x-text="method" class="badge mr-2 last:mr-0" :class="'badge--' + method.toLowerCase()"></li>
+                                            </template>
+                                        </ul>
+                                    </td>
+                                    <td class="whitespace-nowrap px-3 py-4" x-text="route.uri" />
+                                    <td class="whitespace-nowrap px-3 py-4" x-text="route.name" />
+                                    <td class="whitespace-nowrap px-3 py-4" x-text="route.action" />
+                                    <td class="whitespace-nowrap py-4 pl-3 pr-4">
+                                        <ul class="flex">
+                                            <template x-for="method in route.middleware">
+                                                <li x-text="method" class="mr-1 last:mr-0 after:content-[','] last:after:content-['']"></li>
+                                            </template>
+                                        </ul>
+                                    </td>
+                                </tr>
+                            </template>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
-    </section>
-</div>
+        </section>
+    </div>
 
-<script>
-    function loadRoutes() {
-        return {
-            data: Object.values(<?php echo $routes; ?>),
-            search: "",
-            get filteredRoutes() {
-                if (this.search.length === 0) return this.data;
-                return this.data.filter((item) => {
-                    const query = this.search.toLowerCase();
+    <script>
+        function loadRoutes() {
+            return {
+                data: Object.values(<?php echo $routes; ?>),
+                search: "",
+                get filteredRoutes() {
+                    if (this.search.length === 0) return this.data;
+                    return this.data.filter((item) => {
+                        const query = this.search.toLowerCase();
 
-                    const itemWithUri = item?.uri?.toLowerCase()?.includes(query);
-                    const itemWithName = item?.name?.toLowerCase()?.includes(query);
-                    const itemWithAction = item?.action?.toLowerCase()?.includes(query);
-                    const itemWithMiddleware = item?.middleware?.includes(query);
+                        const itemWithUri = item?.uri?.toLowerCase()?.includes(query);
+                        const itemWithName = item?.name?.toLowerCase()?.includes(query);
+                        const itemWithAction = item?.action?.toLowerCase()?.includes(query);
+                        const itemWithMiddleware = item?.middleware?.includes(query);
 
-                    if (itemWithUri || itemWithName || itemWithAction || itemWithMiddleware) return item;
-                });
-            }
-        };
-    }
-</script>
+                        if (itemWithUri || itemWithName || itemWithAction || itemWithMiddleware) return item;
+                    });
+                }
+            };
+        }
+    </script>
 </body>
 </html>
