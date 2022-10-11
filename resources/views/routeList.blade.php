6f8693798e6c7bb345e5201aad1673f231252c93
Lars Wolters <lars@avocado-media.nl>
2022-10-11 10:48:17 +0200

    feat: add example route and view

---
+++ resources/views/routeList.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Routes list for @if(config('app.name'))| {{ config('app.name') }}@endif</title>
+</head>
+<body>
+<h1>Route list for</h1>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,5 @@
+<?php
+
+\Illuminate\Support\Facades\Route::get('/test-route', function () {
+    return view('routeList');
+});
